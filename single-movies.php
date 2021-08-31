fd8369843368147009cd5ca781f1a1f8f0b92d0a
Long Thay <thaybunlong20@yahoo.com>
2021-08-31 13:46:45 +0700

    Add 2 methods of query db.

--- single-movies.php
+++ single-movies.php
@@ -4,6 +4,29 @@ while (have_posts()) {
     the_post();
 ?>
     <div class="main-container">
+
+        <?php
+        $query_func = $_GET['query_func'];
+
+        if ($query_func == "mydb") {
+            $mydb = new wpdb('root', '', 'bitnami_wordpress', 'localhost');
+            $limit = 10;
+            $offset = 0;
+            $table = $mydb->prefix . 'product';
+            $sql = "SELECT * FROM {$table} LIMIT %d OFFSET %d";
+            $data = $mydb->get_results($mydb->prepare($sql, $limit, $offset), ARRAY_A); // get_row, get_var
+            echo json_encode($data);
+        } else {
+            global $wpdb;
+            $limit = 10;
+            $offset = 0;
+            $table = $wpdb->prefix . 'posts';
+            $sql = "SELECT * FROM {$table} WHERE `post_type` = 'post' LIMIT %d OFFSET %d";
+            $data = $wpdb->get_results($wpdb->prepare($sql, $limit, $offset), ARRAY_A); // get_row, get_var
+            echo json_encode($data);
+        }
+        ?>
+
         <h1>Hello from single-movies.php</h1>
         <h2><?php the_title(); ?></h2>
         <?php the_content(); ?>
